eb3d357539d903afd7016dd42ee1a44533b16980
Angad Gupta <127626872+Angad-Gupta-2005@users.noreply.github.com>
2024-09-01 01:36:29 +0530

    Storage Permission is done

--- app/src/main/java/com/angad/videoapp/MainActivity.kt
+++ app/src/main/java/com/angad/videoapp/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.angad.videoapp
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.READ_MEDIA_AUDIO
+import android.Manifest.permission.READ_MEDIA_VIDEO
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.angad.videoapp.databinding.ActivityMainBinding
 
@@ -21,6 +25,10 @@ class MainActivity : AppCompatActivity() {
 //        enableEdgeToEdge()
         setContentView(binding.root)
 
+//        request permissions
+        requestRuntimePermission()
+
+
 //        calling the function to set fragment and setting the VideoFragment as a default fragment
         setFragment(VideosFragment())
 
@@ -30,20 +38,61 @@ class MainActivity : AppCompatActivity() {
 //        binding.helloAn.text = "Angad Gupta"
 
         binding.bottomNav.setOnItemSelectedListener {
-             when(it.itemId){
-                 R.id.videoView -> setFragment(VideosFragment())
-                 R.id.folderView -> setFragment(FoldersFragment())
-             }
+            when (it.itemId) {
+                R.id.videoView -> setFragment(VideosFragment())
+                R.id.folderView -> setFragment(FoldersFragment())
+            }
             return@setOnItemSelectedListener true
         }
     }
 
-//    Function to set the fragment
-    private fun setFragment(fragment: Fragment){
+    //    Function to set the fragment
+    private fun setFragment(fragment: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.fragmentFL, fragment)
         transaction.disallowAddToBackStack()
         transaction.commit()
     }
+
+
+    private fun requestRuntimePermission(): Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+//            For android 13 or above
+            ActivityCompat.requestPermissions(this, arrayOf(READ_MEDIA_VIDEO, READ_MEDIA_AUDIO), 13)
+            return false
+        } else {
+//            For version below android 13
+            ActivityCompat.requestPermissions(
+                this, arrayOf(
+                    WRITE_EXTERNAL_STORAGE,
+                    READ_EXTERNAL_STORAGE
+                ), 13
+            )
+            return false
+        }
+        return true
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 13) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted ", Toast.LENGTH_SHORT).show()
+            } else {
+//                ActivityCompat.requestPermissions(this, arrayOf(WRITE_EXTERNAL_STORAGE), 13)
+                Toast.makeText(
+                    this,
+                    "Permission Denied!!, Open the setting and give the permission",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
 
 }
