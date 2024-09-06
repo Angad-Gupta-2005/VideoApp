682cb4e84a8bbaefec99144d94ab3a385379c23b
Angad Gupta <127626872+Angad-Gupta-2005@users.noreply.github.com>
2024-09-06 14:00:27 +0530

    Full screen and repeat button functionality done

--- app/src/main/java/com/angad/videoapp/PlayerActivity.kt
+++ app/src/main/java/com/angad/videoapp/PlayerActivity.kt
@@ -1,11 +1,18 @@
 package com.angad.videoapp
 
+import android.os.Build
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
 import com.angad.videoapp.databinding.ActivityPlayerBinding
 
@@ -17,14 +24,32 @@ class PlayerActivity : AppCompatActivity() {
         lateinit var player: ExoPlayer
         lateinit var playerList: ArrayList<Video>
         var position: Int = -1
+        var repeat: Boolean = false
     }
 
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+//        Hide the title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.attributes.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_NEVER
+
+//        Initialised the binding class
         binding = ActivityPlayerBinding.inflate(layoutInflater)
+
+//        Set the playerActivity Theme
+        setTheme(R.style.playerActivityTheme)
         setContentView(binding.root)
+
+        //        For immersive mode i.e., full screen mode
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+//        For hiding the buttons
+        WindowInsetsControllerCompat(window, binding.root).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -69,9 +94,23 @@ class PlayerActivity : AppCompatActivity() {
 //        next and previous button functionality
         binding.nextBtn.setOnClickListener { nextPrevVideo() }
         binding.prevBtn.setOnClickListener { nextPrevVideo(isNext = false) }
+        binding.repeatBtn.setOnClickListener {
+            if(repeat){
+                repeat = false
+                player.repeatMode = Player.REPEAT_MODE_OFF
+                binding.repeatBtn.setImageResource(R.drawable.ic_repeat_off)
+            }
+            else{
+                repeat = true
+                player.repeatMode = Player.REPEAT_MODE_ONE
+                binding.repeatBtn.setImageResource(R.drawable.ic_repeat_on)
+            }
+        }
     }
 
     private fun createPlayer(){
+        try { player.release() }catch (e: Exception){ e.message}
+
         binding.videoTitle.text = playerList[position].title
         binding.videoTitle.isSelected = true
 //        Creating an object of player
@@ -82,6 +121,14 @@ class PlayerActivity : AppCompatActivity() {
         player.setMediaItem(mediaItem)
         player.prepare()
         playVideo()
+        //    Set functionality of repeat button
+        player.addListener(object: Player.Listener{
+            override fun onPlaybackStateChanged(playbackState: Int){
+                super.onPlaybackStateChanged(playbackState)
+                if (playbackState == Player.STATE_ENDED)
+                    nextPrevVideo()
+            }
+        })
     }
 
 //    Functionality of play and pause of video
@@ -110,17 +157,19 @@ class PlayerActivity : AppCompatActivity() {
     }
 
     private fun setPosition(isIncrement: Boolean = true){
-        if (isIncrement){
-            if(position == playerList.size - 1)
-                position = 0
-            else
-                ++position
-        }
-        else{
-            if (position == 0)
-                position = playerList.size -1
-            else
-                --position
+        if (!repeat){
+            if (isIncrement){
+                if(position == playerList.size - 1)
+                    position = 0
+                else
+                    ++position
+            }
+            else{
+                if(position == 0)
+                    position = playerList.size -1
+                else
+                    --position
+            }
         }
     }
 
